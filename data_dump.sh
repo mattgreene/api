25df0ca287fe677f8d8721e0fcb6bab404f02532
Matt Greene <matt@copperlabs.com>
2024-11-06 09:40:59 -0700

    Remove tz format from helper script in favor of "native" handling in the main python script

--- data_dump.sh
+++ data_dump.sh
@@ -14,8 +14,8 @@ days=$(( (`gdate -d $finish_date +%s` - `gdate -d $start_date +%s`) / (24*3600) 
 for i in $(eval echo "{0..$days}")
 do
     bucket=`date -j -v +${i}d -f "%Y-%m-%d" "${start_date}" "+%Y-%m-%d"`
-    sdate="${bucket}T06:00:00Z"
-    fdate=`date -j -v +$((i+1))d -f "%Y-%m-%d" "${start_date}" "+%Y-%m-%dT07:00:00Z"` # go over 1 hour to blindly handle time changes
+    sdate="${bucket}"
+    fdate=`date -j -v +$((i+1))d -f "%Y-%m-%d" "${start_date}" "+%Y-%m-%d"` # go over 1 hour to blindly handle time changes
     echo $bucket
     output_dir="${output_base_dir}/${bucket}"
     mkdir -p ${output_dir}
